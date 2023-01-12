8e672997eff8d6dd35f8f1ff1d647d384f671f50
it-hms <iatr@hms.se>
2023-01-12 13:31:44 -0500

    Small update

--- docusaurus/src/pages/index.tsx
+++ docusaurus/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Take me to the Quick Start ⏱️
+            Take me to the docs⏱️
           </Link>
         </div>
       </div>
